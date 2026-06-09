788795decdcb0067bbd15636cbad1944485934da
kimuchi <c0a250809a@edu.teu.ac.jp>
2026-06-09 16:17:33 +0900

    練習10:矢印キーで移動

--- pygame_image.py
+++ pygame_image.py
@@ -12,17 +12,30 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img_koukaton = pg.image.load("fig/3.png")
     bg_img_koukaton = pg.transform.flip(bg_img_koukaton, True, False)
+    bg_rct_koukaton = bg_img_koukaton.get_rect()
+    bg_rct_koukaton.center = 300, 200
+    screen.blit(bg_img_koukaton, bg_rct_koukaton)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_list = pg.key.get_pressed()
+        if key_list[pg.K_UP]:
+            bg_rct_koukaton.move_ip((0, -1))
+        if key_list[pg.K_DOWN]:
+            bg_rct_koukaton.move_ip((0, 1))
+        if key_list[pg.K_LEFT]:
+            bg_rct_koukaton.move_ip((-1, 0))
+        if key_list[pg.K_RIGHT]:
+            bg_rct_koukaton.move_ip((1, 0))
 
         x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
         bg_img_flipped = pg.transform.flip(bg_img, True, False)
         screen.blit(bg_img_flipped, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
-        screen.blit(bg_img_koukaton, [300, 200])
+        screen.blit(bg_img_koukaton, bg_rct_koukaton)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
